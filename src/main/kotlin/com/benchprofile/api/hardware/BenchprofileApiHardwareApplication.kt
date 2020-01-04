3f5b777f5c5233c494de7d30145ce987308ea33d
Christopher Quadflieg <christopher.quadflieg@adsoul.com>
2020-01-04 10:21:28 +0100

    Init gradle project

---
+++ src/main/kotlin/com/benchprofile/api/hardware/BenchprofileApiHardwareApplication.kt
@@ -0,0 +1,11 @@
+package com.benchprofile.api.hardware
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BenchprofileApiHardwareApplication
+
+fun main(args: Array<String>) {
+    runApplication<BenchprofileApiHardwareApplication>(*args)
+}
---
+++ src/test/kotlin/com/benchprofile/api/hardware/BenchprofileApiHardwareApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.benchprofile.api.hardware
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BenchprofileApiHardwareApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
